ad98f9835a7066cd67aa0e3966280d902fe3449b
Sudip Das <sudip5428@gmail.com>
2026-05-01 11:58:04 +0530

    code commit for basic fast api endpoint add

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from src.api.heldth import  router as he
+app=FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+origins=[
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(he)
+
+if __name__ == '__main__':
+    uvicorn.run("main:app",host="127.0.0.1",port=8000,reload=True)
