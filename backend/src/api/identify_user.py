a4f398b0c6a4a37d3118cce12db4cbf2b88a79b3
Sudip Das <sudip5428@gmail.com>
2026-05-01 13:23:01 +0530

    code commit for fix appoinment routes

--- backend/src/api/identify_user.py
+++ backend/src/api/identify_user.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
-
+from sqlalchemy.orm import selectinload
 from src.models.migrations import User,Appointment
 from src.config.db import get_db
 from src.models.schemas import IdentifyUserRequest,BookAppointmentRequest, ModifyAppointmentRequest
@@ -35,6 +35,31 @@ async def get_appointments(
         select(Appointment).where(Appointment.user_id == user_id)
     )
     return result.scalars().all()
+
+@router.get("/all_appointments")
+async def get_all_appointments(
+        db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(
+        select(Appointment).options(selectinload(Appointment.user))
+    )
+
+    appointments = result.scalars().all()
+
+    return [
+        {
+            "id": ap.id,
+            "date": ap.date,
+            "time": ap.time,
+            "status": ap.status,
+            "user": {
+                "id": ap.user.id,
+                "name": ap.user.name,
+                "phone": ap.user.phone
+            } if ap.user else None
+        }
+        for ap in appointments
+    ]
 
 @router.delete("/cancel/{appointment_id}")
 async def cancel_appointment(
--- backend/src/models/migrations.py
+++ backend/src/models/migrations.py
@@ -1,13 +1,14 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 from src.config.db import Base
-
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     phone = Column(String, nullable=False, unique=True)
+    appointments = relationship("Appointment", back_populates="user")
 
 
 class Appointment(Base):
@@ -16,4 +17,5 @@ class Appointment(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     date = Column(String, nullable=False)
     time = Column(String, nullable=False)
-    status = Column(String, default="pending")
+    status = Column(String, default="booked")
+    user = relationship("User", back_populates="appointments") 
